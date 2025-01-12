f7903e615979299865435750cb47ea158b6e8c48
Clon_gnZ <mrclonexiti@gmail.com>
2025-01-12 10:51:04 -0600

    Construye solicitud de API

--- literalura/src/main/java/com/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/com/literalura/LiteraluraApplication.java
@@ -1,8 +1,11 @@
 package com.literalura;
 
+import com.literalura.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
@@ -14,5 +17,19 @@ public class LiteraluraApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("****************Prueba**************");
+		//inicio prueba ConsumoAPI
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Ingresar nombre del libro: ");
+
+		var libro = teclado.nextLine();
+		var json = consumoAPI
+				.obtenerDatos("http://gutendex.com/books/?search="+libro.replace(" ","%20"));
+
+		System.out.println("Datos obtenidos: ");
+		System.out.println(json);
+		//fin prueba consumoAPI
+
 	}
 }
